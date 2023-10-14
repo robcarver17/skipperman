5a8d56b02423efac66c70eccb69de5538f05734a
rob <robcarver17@googlemail.com>
2023-10-14 11:59:56 +0100

    started to add pdf reporting

--- interface/reporting/create_pdf_from_marked_up_strings.py
+++ interface/reporting/create_pdf_from_marked_up_strings.py
@@ -1,60 +1,61 @@
 from fpdf import FPDF
-
+from copy import copy
 
 def create_pdf_from_nested_list_of_marked_up_strings(
-    nested_list_of_str: list,
+    nested_list_of_str_column_grouped: list,
+    file_name: str,
     title: str = "",
     font_size: float = 10.0,
-    font: str = "Times",
+    font: str = "Arial",
     landscape: bool = True,
-    column_width: float = 2.0,
-    column_spacing: float = 0.15,
+    column_spacing: float = 8, ## depend on font size?
+    line_height: float = 8 ## depend on font size
+
 ):
     if landscape:
         orientation = "L"
+        page_width=277
     else:
         orientation = "P"
+        page_width=190
 
     ## these are fixed but shuld still be in config?
     pdf = FPDF(format="A4", unit="mm", orientation=orientation)
+    pdf.set_auto_page_break(0)
     pdf.add_page()
 
     # Title
-    pdf.cell()
+    large_font_size = font_size * 2.0
+    pdf.set_font(font, "", large_font_size)
+    ## height will depend on font size, needs some work
+    ## width, whole width of A4? eithier 210 or 297, slightly less or margins
+
+    pdf.cell(page_width, line_height*2, title, ln=1, align='C')  ## feels like these are magic numbers?
+
+
     pdf.set_font(font, "", font_size)
 
     # Here we save what will be the top of each columns
     ybefore = pdf.get_y()
 
-    # First column
+    use_nested_list_of_str_column_group = copy(nested_list_of_str_column_grouped)
 
-    pdf.multi_cell(
-        column_width,
-        0.15,
-        "Mea tamquam constituto no, facete dissentiunt eos no. Eu agam delicata qui, ex mea utinam consetetur. Pro insolens vulputate id. Mea discere eligendi explicari eu, ut fugit soluta eum. Per wisi putant commodo at.",
-    )
+    number_of_columns = len(use_nested_list_of_str_column_group)
+    column_width = (page_width / number_of_columns) - column_spacing
+    column_count = 0
+    while len(use_nested_list_of_str_column_group)>0:
+        list_for_column = use_nested_list_of_str_column_group.pop(0)
+        for line_number, marked_up_text in enumerate(list_for_column):
+            #set_font_for_marked_up_text(marked_up_text=marked_up_text, pdf=pdf)
+            pdf.set_xy(column_count * (column_width + column_spacing) + pdf.l_margin, ybefore+(line_number*line_height))
+            pdf.multi_cell(
+                w=column_width,
+                h=line_height,
+                txt=marked_up_text.string,
+                align='L',
+            )
 
-    # Notice we have to account for the left margin to get the spacing between
-    # columns right.
 
-    pdf.set_xy(column_width + pdf.l_margin + column_spacing, ybefore)
+        column_count+=1
 
-    # Second column
-
-    pdf.multi_cell(
-        column_width,
-        0.15,
-        "Vis at dolores ocurreret splendide. Noster dolorum repudiare vis ei, te augue summo vis. An vim quas torquatos, electram posidonium eam ea, eros blandit ea vel. Reque summo assueverit an sit. Sed nibh conceptam cu, pro in graeci ancillae constituto, eam eu oratio soleat instructior. No deleniti quaerendum vim, assum saepe munere ea vis, te tale tempor sit. An sed debet ocurreret adversarium, ne enim docendi mandamus sea.",
-    )
-
-    pdf.set_xy(2 * (column_width + column_spacing) + pdf.l_margin, ybefore)
-
-    # Third column
-
-    pdf.multi_cell(
-        column_width,
-        0.15,
-        "Lorem ipsum dolor sit amet, vel ne quando dissentias. Ne his oporteat expetendis. Ei tantas explicari quo, sea vidit minimum menandri ea. His case errem dicam ex, mel eruditi tibique delicatissimi ut. At mea wisi dolorum contentiones, in malis vitae viderer mel.",
-    )
-
-    pdf.output("/home/rob/multi_cell_3_cols.pdf", "F")
+    pdf.output(file_name, "F")

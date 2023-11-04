e22e2a814656d25c185a2c4459b3243b009b10e1
Robert Carver <robcarver17@users.noreply.github.com>
2023-11-04 11:45:46 +0000

    Delete build/lib directory

--- build/lib/app/data_access/classes/wa_event_mapping.py
+++
@@ -1,9 +0,0 @@
-from app.objects.wa_event_mapping import WAEventMapping
-
-
-class DataWAEventMapping(object):
-    def read(self) -> WAEventMapping:
-        raise NotImplemented
-
-    def write(self, wa_event_mapping: WAEventMapping):
-        raise NotImplemented
--- build/lib/app/interface/html/view_pd.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-def view_df_as_html(df: pd.DataFrame):
-    ## Consider adding sort by buttons
-    return df.to_html()

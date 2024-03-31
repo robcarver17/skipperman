0debfa6ec3832492668c823c7a02005d6d7f7179
Rob Carver <robcarver17@gmail.com>
2024-03-31 14:24:23 +0100

    fix annoying list error

--- app/backend/data/qualification.py
+++ app/backend/data/qualification.py
@@ -69,7 +69,7 @@ def load_list_of_cadets_with_qualifications() -> ListOfCadetsWithQualifications:
 def save_list_of_cadets_with_qualifications(list_of_cadets_with_qualifications: ListOfCadetsWithQualifications):
     data.data_list_of_cadets_with_qualifications.write(list_of_cadets_with_qualifications)
 
-def update_qualifications_for_cadet(cadet: Cadet, list_of_qualification_names_for_this_cadet: list[str]):
+def update_qualifications_for_cadet(cadet: Cadet, list_of_qualification_names_for_this_cadet: List[str]):
     list_of_cadets_with_qualifications = load_list_of_cadets_with_qualifications()
     list_of_qualifications = load_list_of_qualifications()
     list_of_qualification_ids = [list_of_qualifications.id_given_name(qual_name) for qual_name in list_of_qualification_names_for_this_cadet]

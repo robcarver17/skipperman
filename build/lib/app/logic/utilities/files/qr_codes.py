ee0b5b0ad8a5c5113f3d9655e3fde426be14b562
Rob Carver <robcarver17@gmail.com>
2024-06-03 21:49:10 +0100

    qr codes

--- build/lib/app/logic/utilities/files/qr_codes.py
+++ build/lib/app/logic/utilities/files/qr_codes.py
@@ -1,7 +1,8 @@
 import os
 import qrcode
+import qrcode.image.svg
 
-from app.data_access.file_access import web_pathname_of_file
+from app.data_access.file_access import web_pathname_of_file, download_directory
 
 from app.logic.utilities.files.render_files import type_directory_and_filename_from_button_name
 
@@ -10,6 +11,13 @@ from app.objects.abstract_objects.abstract_form import File
 
 def generate_qr_code(button_pressed: str) -> File:
     type, directory, filename = type_directory_and_filename_from_button_name(button_pressed)
-    full_filename = os.path.join(directory, filename)
     web_path = web_pathname_of_file(filename)
-    
+    img = qrcode.make(web_path, image_factory=qrcode.image.svg.SvgImage)
+    qr_code_filename = temp_qr_code_file_name(filename)
+    with open(qr_code_filename, 'wb') as qr:
+        img.save(qr)
+
+    return File(qr_code_filename)
+
+def temp_qr_code_file_name(filename:str) -> str:
+    return os.path.join(download_directory, "temp_qr_code_%s.svg" % filename)

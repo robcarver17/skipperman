04cbf7a00438731b6d9af89bedd40524d4312482
Rob Carver <robcarver17@gmail.com>
2024-06-05 16:47:35 +0100

    typo in axis

--- app/backend/food.py
+++ app/backend/food.py
@@ -255,7 +255,7 @@ def get_allergy_list_as_df_for_cadets_and_volunteers(interface: abstractInterfac
 
     volunteer_df = get_allergy_list_as_df_for_volunteers(interface=interface, event=event, food_required_str=food_required_str)
     cadet_df = get_allergy_list_as_df_for_cadets(interface=interface, event=event, food_required_str=food_required_str)
-    both_df = pd.concat([cadet_df, volunteer_df], axis=1)
+    both_df = pd.concat([cadet_df, volunteer_df], axis=0)
 
     header_line = pd.Series(dict(type='', name='%s (%d)' % (food_required_str, len(both_df))))
 

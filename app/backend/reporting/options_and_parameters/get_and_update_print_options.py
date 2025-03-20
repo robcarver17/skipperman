59773383108c75c3fed63c7543cf19a4af079ac1
rob <rob@systematicmoney.org>
2025-03-20 09:41:55 +0000

    fix implement #53

--- app/backend/reporting/options_and_parameters/get_and_update_print_options.py
+++ app/backend/reporting/options_and_parameters/get_and_update_print_options.py
@@ -5,7 +5,7 @@ from app.data_access.store.object_store import ObjectStore
 
 def get_default_print_options(object_store: ObjectStore, report_name: str) -> PrintOptions:
     return object_store.get(
-        object_definition_for_print_options, report_name="default_%s" % report_name
+        object_definition_for_print_options, report_name="%s_default" % report_name
     )
 
 
@@ -16,8 +16,9 @@ def get_print_options(object_store: ObjectStore, report_name: str) -> PrintOptio
 
 
 def reset_print_options_to_default(object_store: ObjectStore, report_name: str):
+    print_options = get_default_print_options(object_store=object_store, report_name=report_name)
     update_print_options(
-        object_store=object_store, report_name=report_name, print_options=PrintOptions()
+        object_store=object_store, report_name=report_name, print_options=print_options
     )
 
 

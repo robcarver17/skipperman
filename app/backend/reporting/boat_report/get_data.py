093a2e38cd2e0828a25d68ab6309404e838a42b1
rob <rob@systematicmoney.org>
2025-06-27 15:25:30 +0100

    changed sail number sort

--- app/backend/reporting/boat_report/get_data.py
+++ app/backend/reporting/boat_report/get_data.py
@@ -111,7 +111,7 @@ def get_df_for_day_of_boat_report_of_cadets(
     if len(df) == 0:
         return pd.DataFrame()
 
-    df = df.sort_values(by=[FIRST_CADET, BOAT_CLASS])
+    df = df.sort_values(by=[FIRST_CADET, BOAT_CLASS, SAIL_NUMBER])
 
     return df
 
@@ -147,7 +147,7 @@ def get_df_for_day_of_boat_report_of_volunteers(
     if len(df) == 0:
         return pd.DataFrame()
 
-    df = df.sort_values(by=[FIRST_CADET,SAIL_NUMBER])
+    df = df.sort_values(by=[SAIL_NUMBER])
 
     return df
 
